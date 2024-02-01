04939b1a4747fbc8a11496744fb06b37750552af
Andre <andrepontde@gmail.com>
2024-02-01 19:46:22 +0000

    Second version of code with user input and loop for multiple queries

--- main.py
+++ main.py
@@ -6,29 +6,43 @@ import os
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 #Calling the secret OPENAI_API_KEY from my system enviroment, this is not necesary, but this approach is more secure. 
 
-completion = client.chat.completions.create(
-  #Using the chat completition API, to be able to call a pretrained model.
-  model="gpt-3.5-turbo",
-  #Selection of the model, this can be replaced to make it more powerfull at a higher token cost.
-  messages=[
-    {"role": "system", "content": "You are an experienced psychologist, familiar with the work from Sigmun Freud."},
-    {"role": "user", "content": "Tell me an interesting theory."}],
-    #Determining the role of the assistant and the prompt from the user to be able to receive an appropiate response.
+
+messages=[{"role": "system", "content": "You are an experienced psychologist, familiar with the work from Sigmun Freud."}]
+#Determining the role of the assistant 
+#It is declared outside the loop so the GPT does not receive the same instruction multiple times.
+
+
+response = ""
+#In this variable the model's answer will be registered and printed later.
+
+while True:
+
+  userInput = input("André: ")
+  #Declaring the variable "userInput" to be able to communicate dinamically with the model in the terminal.
+  messages.append({"role": "user", "content": userInput}),
+  #Asigning a prompt from the user.
+  
+  completion = client.chat.completions.create(
+    #Using the chat completition API, to be able to call a pretrained model.
+    model="gpt-3.5-turbo-0125",
+    #Selection of the model, this can be replaced to make it more powerfull at a higher token cost.
+    messages=messages,
     temperature=0.5,
     #Temperature sets the amount of randomization the model could generate, a higher value results on more creative but inaccurate results, and viceversa.
-    max_tokens=64
+    max_tokens=32
     #Enables a limit on the amount of tokens that can be used by the model, that stuff doesn't grow on trees!
-    
-)
-
-print(completion.choices[0].message)
+  )
+  
+  response = completion.choices[0].message
+  messages.append(response)
+  print(response)
+  #Prints only the response, and declares all of the variables used as well.
 
 
 
 #The following code is an unsuccesfull attempt to call the streaming function ->
 
 
-#Prints the full message, and declares all of the variables used for the response as well.
 
 # from openai import OpenAI
 
